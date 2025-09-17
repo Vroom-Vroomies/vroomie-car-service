9283eebe5b844954be5d23f45a03308d57b95143
sortofel <yeoik731@gmail.com>
2025-09-17 17:33:08 +0900

    - clarify service class by add comment - give domain repository dependency

--- src/main/java/com/vroomie/car_service/domain/fleet/accident/service/AccidentService.java
+++ src/main/java/com/vroomie/car_service/domain/fleet/accident/service/AccidentService.java
@@ -1,13 +1,14 @@
 package com.vroomie.car_service.domain.fleet.accident.service;
 
+import com.vroomie.car_service.domain.employee.repository.EmployeeRepository;
 import com.vroomie.car_service.domain.fleet.accident.dto.AccidentCreateRequestDTO;
 import com.vroomie.car_service.domain.fleet.accident.dto.AccidentDetailDTO;
-import com.vroomie.car_service.domain.fleet.accident.dto.AccidentFinalizeRequestDTO;
 import com.vroomie.car_service.domain.fleet.accident.dto.AccidentListResponseDTO;
 import com.vroomie.car_service.domain.fleet.accident.entity.AccidentEntity;
 import com.vroomie.car_service.domain.fleet.accident.enums.AccidentType;
 import com.vroomie.car_service.domain.fleet.accident.mapper.AccidentMapper;
 import com.vroomie.car_service.domain.fleet.accident.repository.AccidentRepository;
+import com.vroomie.car_service.domain.fleet.car.repository.CarRepository;
 import com.vroomie.car_service.global.exception.BusinessException;
 import com.vroomie.car_service.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -22,15 +23,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccidentService {
 
     private final AccidentRepository accidentRepository;
-    // private final CarRepository carRepository;
-    // private final EmployeeRepository employeeRepository;
+    private final CarRepository carRepository;
+    private final EmployeeRepository employeeRepository;
     private final AccidentMapper accidentMapper;
 
+    // 생성
     @Transactional
     public AccidentDetailDTO createAccident(AccidentCreateRequestDTO req) {
         AccidentEntity accident = AccidentEntity.builder()
-                .car(null) // carRepository.findById(dto.getCarId()).orElseThrow()
-                .employee(null) // employeeRepository.findById(dto.getEmpEmail()).orElseThrow())
+                .car(carRepository.findById(req.getCarId()).orElseThrow())
+                .employee(employeeRepository.findByEmail(req.getEmpEmail()).orElseThrow())
                 .type(AccidentType.valueOf(req.getType()))
                 .note(req.getNote())
                 .detail(req.getDetail())
@@ -42,6 +44,7 @@ public class AccidentService {
         return accidentMapper.toDetailResponse(accidentRepository.save(accident));
     }
 
+    // 상세 조회
     public AccidentDetailDTO getAccidentById(Long id) {
         AccidentEntity accident = accidentRepository.findById(id)
                 .orElseThrow(() -> new BusinessException(ErrorCode.ACCIDENT_NOT_FOUND));
@@ -51,24 +54,27 @@ public class AccidentService {
         return accidentMapper.toDetailResponse(accident);
     }
 
+    // 전체 조회
     public Page<AccidentListResponseDTO> getAllAccidents(Long carId, boolean save, Pageable pageable) {
         Page<AccidentEntity> accidents = accidentRepository.findAll(pageable);
         // TODO. 필터 처리 추가
 
-        return accidentMapper.toListResponse(accidents);
+        return accidents.map(accidentMapper::toListResponseDTO);
     }
 
+    // 수정
     public AccidentDetailDTO updateAccident(Long id, AccidentCreateRequestDTO req) {
         AccidentEntity accident = accidentRepository.findById(id)
                 .orElseThrow(() -> new BusinessException(ErrorCode.ACCIDENT_NOT_FOUND));
 
         checkFinalized(accident);
 
-        accident.update(req);
+        accidentMapper.update(req, accident);
 
         return accidentMapper.toDetailResponse(accident);
     }
 
+    // 최종 제출
     public AccidentDetailDTO finalizeAccident(Long id, boolean save) {
 
         if (!save) {
@@ -84,7 +90,7 @@ public class AccidentService {
         return accidentMapper.toDetailResponse(accident);
     }
 
-    // 최종 저장 여부 확인
+    // 헬퍼 메소드 - 최종 저장 여부 확인
     private void checkFinalized(AccidentEntity accident) {
         if (accident.isSaved()) {
             throw new BusinessException(ErrorCode.ACCIDENT_ALREADY_FINALIZED);
