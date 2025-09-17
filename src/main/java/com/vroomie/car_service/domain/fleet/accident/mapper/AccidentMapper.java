11aa1a33a59c0212d088dc3090a73524d4145167
sortofel <yeoik731@gmail.com>
2025-09-17 17:31:30 +0900

    fix mapping error (syntax)

--- src/main/java/com/vroomie/car_service/domain/fleet/accident/mapper/AccidentMapper.java
+++ src/main/java/com/vroomie/car_service/domain/fleet/accident/mapper/AccidentMapper.java
@@ -1,31 +1,30 @@
 package com.vroomie.car_service.domain.fleet.accident.mapper;
 
+import com.vroomie.car_service.domain.fleet.accident.dto.AccidentCreateRequestDTO;
 import com.vroomie.car_service.domain.fleet.accident.dto.AccidentDetailDTO;
 import com.vroomie.car_service.domain.fleet.accident.dto.AccidentListResponseDTO;
 import com.vroomie.car_service.domain.fleet.accident.entity.AccidentEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
-import org.mapstruct.factory.Mappers;
-import org.springframework.data.domain.Page;
+import org.mapstruct.*;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AccidentMapper {
-    AccidentMapper INSTANCE = Mappers.getMapper(AccidentMapper.class);
 
     @Mappings({
-            @Mapping(source = "car_id", target = "carId"),
-            @Mapping(source = "emp_email", target = "empEmail"),
-            @Mapping(source = "occurred_at", target = "date"),
-            @Mapping(source = "is_saved", target = "save")
+            @Mapping(source = "car.id", target = "carId"),
+            @Mapping(source = "employee.email", target = "empEmail"),
+            @Mapping(source = "occurredAt", target = "date"),
+            @Mapping(source = "saved", target = "save")
     })
     AccidentDetailDTO toDetailResponse(AccidentEntity accident);
 
     @Mappings({
-            @Mapping(source = "car_id", target = "carId"),
-            @Mapping(source = "emp_email", target = "empEmail"),
-            @Mapping(source = "occurred_at", target = "date"),
-            @Mapping(source = "is_saved", target = "save")
+            @Mapping(source = "car.id", target = "carId"),
+            @Mapping(source = "employee.email", target = "empEmail"),
+            @Mapping(source = "occurredAt", target = "date"),
+            @Mapping(source = "saved", target = "save")
     })
-    Page<AccidentListResponseDTO> toListResponse(Page<AccidentEntity> accidents);
+    AccidentListResponseDTO toListResponseDTO(AccidentEntity accident);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void update(AccidentCreateRequestDTO req, @MappingTarget AccidentEntity accident);
 }
