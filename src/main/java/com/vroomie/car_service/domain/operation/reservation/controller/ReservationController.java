2dccd4695ac1a5d4263de4d80d2742a4766f12c9
lalabong <jiwoni1210@naver.com>
2025-09-18 11:54:40 +0900

    chore: ReservationController API 문서 설명 개선

--- src/main/java/com/vroomie/car_service/domain/operation/reservation/controller/ReservationController.java
+++ src/main/java/com/vroomie/car_service/domain/operation/reservation/controller/ReservationController.java
@@ -22,13 +22,13 @@ import jakarta.validation.constraints.Max;
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
-@Tag(name = "예약 관리")
+@Tag(name = "대여 신청 관리")
 public class ReservationController {
 
     private final ReservationService reservationService;
 
     // [관리자] 대여 신청 목록 조회
-    @Operation(summary = "대여 신청 목록 조회", description = "대여 신청 목록을 조회합니다.")
+    @Operation(summary = "[관리자]대여 신청 목록 조회", description = "대여 신청 목록을 조회합니다.")
     @GetMapping("/admin")
     public ApiResponse<PageResponse<AdminReservationResponse>> getAdminReservationList(
             @RequestParam(defaultValue = "1") @Min(value = 1, message = "페이지 번호는 1 이상이어야 합니다.") int currentPage,
@@ -40,12 +40,14 @@ public class ReservationController {
     }
 
     // [관리자] 대여 신청 상태 변경(승인 or 거절)
-    @Operation(summary = "대여 신청 상태 변경", description = "대여 신청 상태를 변경합니다.")
+    @Operation(summary = "[관리자]대여 신청 상태 변경", description = "대여 신청 상태를 변경합니다.")
     @PutMapping("/admin/{id}")
     public ApiResponse<AdminReservationResponse> updateAdminReservationStatus(
             @PathVariable @Min(value = 1, message = "대여 신청 ID는 1 이상이어야 합니다.") Long id,
             @RequestBody AdminReservationRequest request) {
         return ApiResponse.success(reservationService.updateAdminReservationStatus(id, request), "대여 신청 상태 변경 성공");
     }
+
+
 
 }
