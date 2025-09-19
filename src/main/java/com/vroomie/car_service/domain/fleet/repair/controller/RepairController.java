31cac8f3a5793cd806b7f2806417460db19dfef2
sortofel <yeoik731@gmail.com>
2025-09-19 17:53:17 +0900

    add swagger description to accident and repair controller

--- src/main/java/com/vroomie/car_service/domain/fleet/repair/controller/RepairController.java
+++ src/main/java/com/vroomie/car_service/domain/fleet/repair/controller/RepairController.java
@@ -7,6 +7,8 @@ import com.vroomie.car_service.domain.fleet.repair.dto.RepairListResponseDTO;
 import com.vroomie.car_service.domain.fleet.repair.enums.RepairStatus;
 import com.vroomie.car_service.domain.fleet.repair.service.RepairService;
 import com.vroomie.car_service.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,11 +22,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/fleet/repairs")
 @RequiredArgsConstructor
+@Tag(name = "수리 기록", description = "수리 기록 관련 API")
 public class RepairController {
 
     private final RepairService repairService;
 
     @PostMapping
+    @Operation(summary = "신규 수리 기록 등록")
     public ResponseEntity<ApiResponse<RepairDetailDTO>> createRepair(
             @RequestPart("req") RepairCreateRequestDTO req,
             @RequestPart(value = "beforeImages", required = false) List<MultipartFile> beforeImages,
@@ -37,6 +41,7 @@ public class RepairController {
     }
 
     @GetMapping
+    @Operation(summary = "전체 수리 목록 조회", description = "전체 사고 목록을 조회합니다. \n 필터: 차 ID, 수리 상태, 최종 제출 여부")
     public ResponseEntity<ApiResponse<Page<RepairListResponseDTO>>> getRepairList(
             @RequestParam(required = false) Long carId,
             @RequestParam(required = false) RepairStatus status,
@@ -50,6 +55,7 @@ public class RepairController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "수리 기록 상세 조회", description = "수리 기록 id로 수리 기록의 상세 정보를 조회합니다")
     public ResponseEntity<ApiResponse<RepairDetailDTO>> getRepairDetail(@PathVariable Long id) {
         RepairDetailDTO data = repairService.getRepairById(id);
 
@@ -57,6 +63,7 @@ public class RepairController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "수리 기록 수정", description = "수리 기록을 입력 받은 최신 정보로 업데이트 합니다")
     public ResponseEntity<ApiResponse<RepairDetailDTO>> updateRepair(
             @PathVariable Long id,
             @RequestPart("req") RepairCreateRequestDTO req,
@@ -69,6 +76,7 @@ public class RepairController {
     }
 
     @PatchMapping("/save/{id}")
+    @Operation(summary = "사고 이력 최종 제출", description = "사고 이력을 최종 제출합니다. 최종 제출 이후에는 수정이 불가능합니다.")
     public ResponseEntity<ApiResponse<RepairDetailDTO>> finalizeRepair(
             @PathVariable Long id,
             @RequestBody RepairFinalizeRequestDTO req) {
