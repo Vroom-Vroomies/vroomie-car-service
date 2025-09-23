61fb5613d9ddf53e32329d5a7f4391c9300bec6d
lalabong <jiwoni1210@naver.com>
2025-09-23 22:09:26 +0900

    feat: 전역 타임존 나라 설정

--- src/main/java/com/vroomie/car_service/CarServiceApplication.java
+++ src/main/java/com/vroomie/car_service/CarServiceApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class CarServiceApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarServiceApplication.class, args);
